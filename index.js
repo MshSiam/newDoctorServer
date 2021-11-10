cbad3e9a4716e87cc04de01794a99a8fc1de8e32
MshSiam <siyamkenway@gmail.com>
2021-11-10 07:54:23 +0600

    improved

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const cors = require("cors")
+require("dotenv").config()
+const { MongoClient } = require("mongodb")
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woosd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 async function run() {
-    try {
-        await client.connect();
-        const database = client.db('doctors_portal');
-        const appointmentsCollection = database.collection('appointments');
+  try {
+    await client.connect()
+    const database = client.db("doctors_potral")
+    const appointmentsCollection = database.collection("appointsments")
 
-        app.get('/appointments', async (req, res) => {
-            const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+    app.get("/appointsments", async (req, res) => {
+      const email = req.query.email
+      const date = new Date(req.query.date).toLocaleDateString()
 
-            const query = { email: email, date: date }
+      const query = { email: email, date: date }
 
-            const cursor = appointmentsCollection.find(query);
-            const appointments = await cursor.toArray();
-            res.json(appointments);
-        })
+      const cursor = appointmentsCollection.find(query)
+      const appointments = await cursor.toArray()
+      res.json(appointments)
+    })
 
-        app.post('/appointments', async (req, res) => {
-            const appointment = req.body;
-            const result = await appointmentsCollection.insertOne(appointment);
-            console.log(result);
-            res.json(result)
-        });
-
-    }
-    finally {
-        // await client.close();
-    }
+    app.post("/appointsments", async (req, res) => {
+      const appointment = req.body
+      const result = await appointmentsCollection.insertOne(appointment)
+      console.log(result)
+      res.json(result)
+    })
+  } finally {
+    // await client.close();
+  }
 }
 
-run().catch(console.dir);
+run().catch(console.dir)
 
-app.get('/', (req, res) => {
-    res.send('Hello Doctors portal!')
+app.get("/", (req, res) => {
+  res.send("Hello Doctors portal!")
 })
 
 app.listen(port, () => {
-    console.log(`listening at ${port}`)
+  console.log(`listening at ${port}`)
 })
 
 // app.get('/users')
